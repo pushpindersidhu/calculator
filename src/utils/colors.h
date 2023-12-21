80aefe3d1325f86f9678d9ca789fa5a14569deda
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-12-21 09:52:16 -0800

    Add colors.h for console text formatting

---
+++ src/utils/colors.h
@@ -0,0 +1,26 @@
+//
+// Created by Pushpinder Sidhu on 2023-12-21.
+//
+
+#ifndef CALCULATOR_COLORS_H
+#define CALCULATOR_COLORS_H
+
+#define Reset   "\033[0m"
+#define Black   "\033[30m"
+#define Red     "\033[31m"
+#define Green   "\033[32m"
+#define Yellow  "\033[33m"
+#define Blue    "\033[34m"
+#define Magenta "\033[35m"
+#define Cyan    "\033[36m"
+#define White   "\033[37m"
+#define BoldBlack   "\033[1m\033[30m"
+#define BoldRed     "\033[1m\033[31m"
+#define BoldGreen   "\033[1m\033[32m"
+#define BoldYellow  "\033[1m\033[33m"
+#define BoldBlue    "\033[1m\033[34m"
+#define BoldMagenta "\033[1m\033[35m"
+#define BoldCyan    "\033[1m\033[36m"
+#define BoldWhite   "\033[1m\033[37m"
+
+#endif //CALCULATOR_COLORS_H
