ccde9fc73986595b67727878c4daa826cf52e2b1
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-12-21 10:13:27 -0800

    Refactor smath to use namespace

--- src/math/smath.h
+++ src/math/smath.h
@@ -5,40 +5,38 @@
 #ifndef Calculator_MATH_H
 #define Calculator_MATH_H
 
+namespace Math {
+    extern const double PI;
+    extern const double E;
 
-class Math {
-public:
-    static const double PI;
-    static const double E;
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    int divide(int a, int b);
 
-    static int add(int a, int b);
-    static int subtract(int a, int b);
-    static int multiply(int a, int b);
-    static int divide(int a, int b);
+    double sine(double a);
+    double cosine(double a);
+    double tangent(double a);
+    double cotangent(double a);
+    double secant(double a);
+    double cosecant(double a);
 
-    static double sine(double a);
-    static double cosine(double a);
-    static double tangent(double a);
-    static double cotangent(double a);
-    static double secant(double a);
-    static double cosecant(double a);
+    double naturalLog(double a);
+    double logBase10(double a);
+    double logBaseN(double a, double n);
 
-    static double naturalLog(double a);
-    static double logBase10(double a);
-    static double logBaseN(double a, double n);
+    double exponent(double a);
+    double exponentBaseN(double a, double n);
 
-    static double exponent(double a);
-    static double exponentBaseN(double a, double n);
-
-    static double squareRoot(double a);
-    static double cubeRoot(double a);
-    static double nthRoot(double a, double n);
-    static double factorial(int a);
-    static double absoluteValue(double a);
-    static double power(double a, double b);
-    static double inverse(double a);
-    static double percent(double a, double b);
-    static double radians(double a);
+    double squareRoot(double a);
+    double cubeRoot(double a);
+    double nthRoot(double a, double n);
+    double factorial(int a);
+    double absoluteValue(double a);
+    double power(double a, double b);
+    double inverse(double a);
+    double percent(double a, double b);
+    double radians(double a);
 };
 
 
